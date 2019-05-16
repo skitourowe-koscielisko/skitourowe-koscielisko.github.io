76f6e4d5a0fa78be3e0478aa13c11098f5631a11
Krzysztof Walo <krzysztof.walo@gmail.com>
2019-05-16 14:57:39 +0200

    WIP on landing banner

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,6 +1,9 @@
 (function() {
     var opacity = 1.0;
+    var paddingRight = 0;
     const header = document.querySelector('article header');
+
+
     window.addEventListener('scroll', function(event) {
         const newOpacity = 1 - (event.pageY / (window.innerHeight / 2)).toFixed(1);
         if (newOpacity < 0)
@@ -10,5 +13,23 @@
             header.style = `opacity: ${opacity}`;
         }
     });
+
+    // Can't do this in CSS :(
+    shiftParent();
+    window.addEventListener('resize', shiftParent);
+    function shiftParent() {
+        const minWidth = 1024;
+        var width = document.body.clientWidth + paddingRight;
+        console.log('resize', document.body.clientWidth, document.body.classList);
+        if (width > 1024 && document.body.classList.contains('landing'))
+        {
+            document.body.parentElement.style.paddingRight = '90px';
+            paddingRight = 90;
+        } else {
+            document.body.parentElement.style.paddingRight = '0px';
+            paddingRight = 0;
+        }
+
+    }
 })();
 
